301a89ee1af43d44ec5385957b8561aa7b32c576
irenako <i.irenakoval@gmail.com>
2022-11-30 15:47:32 +0200

    vscode course

---
+++ src/components/AwesomeComponent.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+import { capitalize } from '@/helper.ts';
+</script>
+
+<template>
+  <div>
+    <p class="fa-twitter"> Hello world</p>
+  </div>
+</template>
+
+<style scoped>
+div {
+  padding: 10px;
+}
+</style>
